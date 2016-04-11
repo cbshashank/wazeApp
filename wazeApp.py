6ac8d137ca3d3ac5edde27c8a7f5496dad69fc37
cbshashank <shashank@Shashanks-MacBook-Pro.local>
2016-04-10 22:35:54 -0400

    minor changes to UI

--- wazeApp.py
+++ wazeApp.py
@@ -1,7 +1,8 @@
 
 import loaddata
 from flask import Flask, render_template, request
-
+nodedirectory="/Users/shashank/Documents/school/Spring2016/ds2/wazeApp/cal.cnode.txt"
+edgedirectory="/Users/shashank/Documents/school/Spring2016/ds2/wazeApp/cal.cedge.txt"
 coordinateDict={}
 edgeConnections={}
 edgeDistance={}
@@ -14,13 +15,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    vertices = loaddata.loadNodes("/Users/shashank/Documents/school/Spring2016/ds2/wazeApp/cal.cnode.txt")
+    vertices = loaddata.loadNodes(nodedirectory)
     return render_template("waze.html", vertices=vertices)
 
 
 @app.route('/test')
 def test():
-    vertices = loaddata.loadNodes("/Users/shashank/Documents/school/Spring2016/ds2/wazeApp/cal.cnode.txt")
+    vertices = loaddata.loadNodes(nodedirectory)
     return render_template("test.html", vertices=vertices)
 
 
